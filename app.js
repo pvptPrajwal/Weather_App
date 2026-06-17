4d9aae3208df0e83642c904e2e3e921e4fba964f
pvptPrajwal <binorkarprajwal1@gmail.com>
2026-06-17 16:52:31 +0530

    local storage

--- app.js
+++ app.js
@@ -12,11 +12,17 @@ searchBtn.addEventListener("click",async () =>{
     const result = await weatherNow(value);
     exactLocation.innerText =`${result.location.name} ${result.location.region} ${result.location.country}`;   
     time.innerText =`${result.location.localtime}`;
-    temp.innerText = `${result.current.temp_c}°C`;
-    
+    temp.innerText = `${result.current.temp_c}°C`;    
+    localStorage.setItem("Location",`${result.location.name} ${result.location.region} ${result.location.country}`);
+    localStorage.setItem("Time",`${result.location.localtime}`);
+    localStorage.setItem("temp",`${result.current.temp_c}`);
 });
 
 async function weatherNow(loc){
     const data =await fetch(`https://api.weatherapi.com/v1/current.json?key=ed608fa4233d4779a4164901261606&q=${loc}&aqi=yes`)
     return await data.json();
 }
+
+window.addEventListener("load",() =>{
+    const value = localStorage.getItem("Location");
+});
