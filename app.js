675803bc37c1840c818898aadeb80841c5f8f7cc
pvptPrajwal <binorkarprajwal1@gmail.com>
2026-06-17 14:55:49 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+// https://api.weatherapi.com/v1/current.json?key=ed608fa4233d4779a4164901261606&q=London&aqi=yes
+const textBox = document.querySelector("#search-City");
+const searchBtn = document.querySelector("#Search-btn");
+
+const exactLocation = document.querySelector("#loction");
+const temp = document.querySelector("#temp");
+const time = document.querySelector("#date-time");
+
+searchBtn.addEventListener("click",async () =>{
+    const value =textBox.value;
+    console.log(value);
+    const result = await weatherNow(value);
+    exactLocation.innerText =`${result.location.name} ${result.location.region} ${result.location.country}`;   
+    time.innerText =`${result.location.localtime}`;
+    temp.innerText = `${result.current.temp_c}°C`;
+    
+});
+
+async function weatherNow(loc){
+    const data =await fetch(`https://api.weatherapi.com/v1/current.json?key=ed608fa4233d4779a4164901261606&q=${loc}&aqi=yes`)
+    return await data.json();
+}
